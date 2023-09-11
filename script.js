554255b298adad86e0b6d917a4469b4cccd576b9
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-09-11 20:38:19 -0300

    add open and close list

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-/* OPEN AND CLOSE BUTTON LI SELECT (mass, temperature, length) AND CLOSE LI*/
-const btnSelectMeasure = document.querySelector(".btn_select_option");
-const listOfMeasure = document.querySelector(".list");
-const items = listOfMeasure.querySelectorAll("li");
+const allButtons = document.querySelectorAll("button")
 
-btnSelectMeasure.addEventListener("click", () => {
-  listOfMeasure.style.display = 'block'
+allButtons.forEach((btn) => {
+  btn.addEventListener('click', openList);
 })
 
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    const selectedItemText = item.textContent
-    btnSelectMeasure.textContent = selectedItemText
-    listOfMeasure.style.display = 'none'
-  })
-})
+function openList(btn) {
+  const ul = btn.currentTarget.nextElementSibling;
 
-document.addEventListener("click", (event) => {
-  if (event.target !== btnSelectMeasure && !listOfMeasure.contains(event.target)) {
-    listOfMeasure.style.display = 'none'
+  if (ul.style.display == 'block') {
+    ul.style.display = 'none'
+  } else {
+    ul.style.display = 'block'
   }
-})
-
-
-/* OPEN AND CLOSE BUTTON SELECT MEASURE X MEASURE AND CLOSE LI*/
-
+}
